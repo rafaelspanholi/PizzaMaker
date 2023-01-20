54542ee76bb48c675fc29ee45de3628d9798e6b6
rafaelspanholi <rafaelles@gmail.com>
2023-01-20 14:44:44 -0300

    criar funcao resetarTudo e botao de carrinho (provisório)

--- app.js
+++ app.js
@@ -1,33 +1,37 @@
+import { onEvent, showElement, setScreen } from "./code.org.js";
 import {
-    onEvent, 
-    showElement, 
-    setScreen} from './code.org.js';
-import {
-escolherMassa,
-escolherMolho,
-escolherTipo
- } from './escolhas.js';
-
+  escolherMassa,
+  escolherMolho,
+  escolherTipo,
+  resetarTudo,
+} from "./escolhas.js";
 
 //Quando 1 das 3 opções estiver marcada o botão create irá aparecer
 onEvent("radioBase1", "click", function () {
-    showElement("buttonCreate");
+  showElement("buttonCreate");
 });
 onEvent("radioBase2", "click", function () {
-    showElement("buttonCreate");
+  showElement("buttonCreate");
 });
 onEvent("radioBase3", "click", function () {
-    showElement("buttonCreate");
+  showElement("buttonCreate");
 });
-//Quando o botão create for clicado vai para prox tela 
+//Quando o botão create for clicado vai para prox tela
 //mostrando o resultado da pizza baseado nas opções escolhidas
 onEvent("buttonCreate", "click", function () {
-    setScreen("screenPizza");
-    escolherMassa();
-    escolherMolho();
-    escolherTipo();
+  setScreen("screenPizza");
+  escolherMassa();
+  escolherMolho();
+  escolherTipo();
+});
+
+onEvent("buttonCart", "click", function () {
+  //let pizza = getPizza();
+  //Pizzas.push(pizza);
+
+  resetarTudo();
 });
 
 onEvent("buttonBack", "click", function () {
-    setScreen("screenMaker");
+  setScreen("screenMaker");
 });
